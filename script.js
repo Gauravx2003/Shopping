29867f7d0fc5ef7b9682d039bff4bd1b43f597be
Gauravx2003 <gauravdaware007@gmail.com>
2024-06-19 10:54:50 +0530

    More Changes

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 const bar=document.getElementById("bar");
 const nav=document.getElementById("navbar");
+const navs=document.querySelectorAll('#navbar');
 const close=document.getElementById("close");
+
+const second=navs[1];
+
+
+// console.log(nav);
+
 
 if (bar){
     bar.addEventListener('click', ()=> {
+        while(second.firstChild){
+            nav.appendChild(second.firstChild);
+        }
         nav.classList.add('active');
     })
 }
@@ -13,3 +23,25 @@ if (close){
         nav.classList.remove('active');
     })
 }
+
+
+/*var main_i=document.getElementById("main-img");
+var shopimg=document.querySelectorAll(".pro");
+console.log(shopimg);
+
+function addClickListeners(elements) {
+    elements.forEach(element => {
+        element.addEventListener('click', function() {
+            const temp=element.querySelector('img');
+            console.log(temp.src);
+            console.log(main_img);
+            main_img.src=temp.src;
+            window.location.href='sproduct.html';
+            
+            // Perform any other actions you want here
+        });
+    });
+}
+
+// Add click event listeners to the array of elements
+addClickListeners(shopimg);*/
